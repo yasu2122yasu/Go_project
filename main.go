8de2e68f790f6ec4cfdafb85a329ba987c1a83f4
yasu2122yasu <ya2su1shi2@icloud.com>
2022-05-08 18:07:33 +0900

    依存性注入を行う

--- main.go
+++ main.go
@@ -12,20 +12,18 @@ import (
 func main() {
 	app := iris.New()
 
-	// ミドルウェアの使用
 	app.Use(iris.Compression)
 	app.Configure(iris.WithoutBodyConsumptionOnUnmarshal)
 
-	// ログ記録（これも備え付きミドルウェア）
+	// ログ記録（これも備えつきのミドルウェア）
 	ac := accesslog.File("./access.log")
 	defer ac.Close()
 	app.UseRouter(ac.Handler)
 	app.UseRouter(recover.New())
 
-	// "/users/"から始まるURLを受け取った際の処理をグループ化
+	//"/user"から始まるURLを受け取った際の処理をグループにする
 	users := app.Party("/users")
 	mvc.Configure(users, setups.ConfigureUsersControllers)
 
-	// ポートの指定
 	app.Listen(":8080")
 }
